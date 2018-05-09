407c39f1ae6b359858f4fc6114ca547723af5563
MrPAttema <patrickattema@home.nl>
2018-05-09 11:23:04 +0200

    Update CSS

--- resources/views/call/waiting.blade.php
+++ resources/views/call/waiting.blade.php
@@ -2,27 +2,10 @@
 
 @section('content')
 
-    <div class="flex-center position-ref full-height">
-
-        <div class="top-right">
-            @if (session()->has('carenAuthToken'))
-                <form action="/caren/auth/destroy" method="POST">
-                    <button class="btn" type="submit">Uitloggen</button>
-                    {{ csrf_field() }}
-                </form>
-            @endif
-        </div>
-        
+    <div class="flex-center position-ref full-height">  
         
         <CallingComponent></CallingComponent>
 
-    {{-- <style>
-        #outgoing {
-            width: 600px;
-            word-wrap: break-word;
-        }
-    </style>
-    <pre id="outgoing"></pre> --}}
     </div>
 @endsection
 
